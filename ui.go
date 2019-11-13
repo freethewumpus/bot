c3021d2a6a8c53c35ec4e272e7e9e77ea2b28638
Jake Gealer <jake@gealer.email>
2019-11-13 07:57:25 +0000

    Update the UI

--- ui.go
+++ ui.go
@@ -2,111 +2,134 @@ package main
 
 import (
 	"github.com/bwmarrin/discordgo"
+	r "gopkg.in/rethinkdb/rethinkdb-go.v5"
 )
 
 func CreateNewMenu(MenuID string, msg discordgo.Message) *EmbedMenu {
 	MainMenu := NewEmbedMenu(
 		discordgo.MessageEmbed{
-			Title: "Freethewump.us Manager",
+			Title:       "Freethewump.us Manager",
 			Description: "Using this bot, you can manage your domains and tokens. This is also where you will join other domains.",
-			Color: 255,
+			Color:       255,
 		}, &MenuInfo{
 			MenuID: MenuID,
 			Author: msg.Author.ID,
-			Info: []string{},
+			Info:   []string{},
 		},
 	)
 
+	user := GetUser(msg.Author.ID)
+
+	EncryptionName := "Enable Encryption"
+	if user.Encryption {
+		EncryptionName = "Disable Encryption"
+	}
+	MainMenu.Reactions.Add(MenuReaction{
+		button: MenuButton{
+			Description: "This will allow you to toggle encryption.",
+			Name:        EncryptionName,
+			Emoji:       "🔑",
+		},
+		function: func(ChannelID string, MessageID string, menu *EmbedMenu, client *discordgo.Session) {
+			_ = client.MessageReactionsRemoveAll(ChannelID, MessageID)
+			defer CreateNewMenu(MenuID, msg).Display(ChannelID, MessageID, client)
+
+			user.Encryption = !user.Encryption
+			r.Table("users").Get(user.Id).Update(&map[string]interface{}{
+				"encryption": user.Encryption,
+			}).Exec(RethinkConnection)
+		},
+	})
+
 	Domains := MainMenu.NewChildMenu(discordgo.MessageEmbed{
-		Title: "Domain Management",
+		Title:       "Domain Management",
 		Description: "This is where you set your default domain, add new ones and join other public ones.",
-		Color: 255,
+		Color:       255,
 	}, MenuButton{
 		Description: "This is where you set your default domain, manage your domains, and add new ones.",
-		Name: "Domain Management",
-		Emoji: "🕸",
+		Name:        "Domain Management",
+		Emoji:       "🕸",
 	})
 	Domains.AddBackButton()
 	Domains.Reactions.Add(MenuReaction{
-		button:   MenuButton{
-			Emoji: "🗺",
-			Name: "Add Domain",
+		button: MenuButton{
+			Emoji:       "🗺",
+			Name:        "Add Domain",
 			Description: "This will guide you through adding a domain.",
 		},
 		function: AddDomain,
 	})
-	user := GetUser(msg.Author.ID)
 	Domains.Reactions.Add(MenuReaction{
-		button:   MenuButton{
-			Emoji: "👥",
-			Name: "Get Owned Domains",
+		button: MenuButton{
+			Emoji:       "👥",
+			Name:        "Get Owned Domains",
 			Description: "This will get all owned domains.",
 		},
 		function: DomainPages("Owned Domains", user.GetOwnedDomains, 0, ShowDomain),
 	})
 	Domains.Reactions.Add(MenuReaction{
-		button:   MenuButton{
-			Emoji: "👱",
-			Name: "Get Whitelisted Domains",
+		button: MenuButton{
+			Emoji:       "👱",
+			Name:        "Get Whitelisted Domains",
 			Description: "This will get all whitelisted domains.",
 		},
 		function: DomainPages("Whitelisted Domains", user.GetWhitelistedDomains, 0, ShowDomain),
 	})
 	Domains.Reactions.Add(MenuReaction{
-		button:   MenuButton{
-			Emoji: "🌐",
-			Name: "Get Public Domains",
+		button: MenuButton{
+			Emoji:       "🌐",
+			Name:        "Get Public Domains",
 			Description: "This will get all public domains.",
 		},
-		function:  DomainPages("Public Domains", GetPublicDomains, 0, ShowDomain),
+		function: DomainPages("Public Domains", GetPublicDomains, 0, ShowDomain),
 	})
 
 	Tokens := MainMenu.NewChildMenu(discordgo.MessageEmbed{
-		Title: "Tokens Management",
+		Title:       "Tokens Management",
 		Description: "This is where you can add or revoke tokens.",
-		Color: 255,
+		Color:       255,
 	}, MenuButton{
 		Description: "This is where you can add or revoke tokens.",
-		Name: "Token Management",
-		Emoji: "🎟",
+		Name:        "Token Management",
+		Emoji:       "🎟",
 	})
 	Tokens.AddBackButton()
 	Tokens.Reactions.Add(MenuReaction{
-		button:   MenuButton{
-			Emoji: "🥊",
-			Name: "Revoke Tokens",
+		button: MenuButton{
+			Emoji:       "🥊",
+			Name:        "Revoke Tokens",
 			Description: "This will revoke all your tokens.",
 		},
-		function:  TokenInvalidationEmbed,
+		function: TokenInvalidationEmbed,
 	})
 	Tokens.Reactions.Add(MenuReaction{
-		button:   MenuButton{
-			Emoji: "🎟",
-			Name: "Generate Token",
+		button: MenuButton{
+			Emoji:       "🎟",
+			Name:        "Generate Token",
 			Description: "This will generate a token. This requires DM's to be on.",
 		},
-		function:  TokenGenerationEmbed,
+		function: TokenGenerationEmbed,
 	})
 
 	UserInfo := GetUser(msg.Author.ID)
 
 	FileNamingPattern := MainMenu.NewChildMenu(discordgo.MessageEmbed{
-		Title: "File Naming Pattern",
+		Title:       "File Naming Pattern",
 		Description: GeneratePatternDescription(UserInfo),
-		Color: 255,
+		Color:       255,
 	}, MenuButton{
 		Description: "This is where you can set your file naming pattern.",
-		Name: "File Naming Pattern",
-		Emoji: "🗄",
+		Name:        "File Naming Pattern",
+		Emoji:       "🗄",
 	})
 	FileNamingPattern.AddBackButton()
 	FileNamingPattern.Reactions.Add(MenuReaction{
-		button:   MenuButton{
+		button: MenuButton{
 			Description: "This will allow you to change your file naming pattern.",
-			Name: "Change Pattern",
-			Emoji: "🗄",
+			Name:        "Change Pattern",
+			Emoji:       "🗄",
 		},
-		function:  ChangePattern,
+		function: ChangePattern,
 	})
 
 	return &MainMenu
