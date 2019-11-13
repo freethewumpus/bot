29a3cb98505cebd47f689a3595fce4ea5a44c27f
Jake Gealer <jake@gealer.email>
2019-11-13 10:49:52 +0000

    Linting and add encryption

--- structs.go
+++ structs.go
@@ -3,41 +3,42 @@ package main
 import r "gopkg.in/rethinkdb/rethinkdb-go.v5"
 
 type Token struct {
-	Id    string `gorethink:"id,omitempty"`
-	Uid  string `gorethink:"uid"`
+	Id  string `gorethink:"id,omitempty"`
+	Uid string `gorethink:"uid"`
 }
 
 type S3Bucket struct {
-	Endpoint string `gorethink:"endpoint"`
-	Bucket string `gorethink:"bucket"`
-	AccessKeyId string `gorethink:"access_key_id"`
+	Endpoint        string `gorethink:"endpoint"`
+	Bucket          string `gorethink:"bucket"`
+	AccessKeyId     string `gorethink:"access_key_id"`
 	SecretAccessKey string `gorethink:"secret_access_key"`
-	Region string `gorethink:"region"`
+	Region          string `gorethink:"region"`
 }
 
 type Domain struct {
-	Id string `gorethink:"id,omitempty"`
-	Public  bool `gorethink:"public"`
-	Whitelist []string `gorethink:"whitelist"`
-	Blacklist []string `gorethink:"blacklist"`
-	Owner string `gorethink:"owner"`
-	Bucket *S3Bucket `gorethink:"bucket"`
+	Id         string    `gorethink:"id,omitempty"`
+	Public     bool      `gorethink:"public"`
+	Whitelist  []string  `gorethink:"whitelist"`
+	Blacklist  []string  `gorethink:"blacklist"`
+	Owner      string    `gorethink:"owner"`
+	Bucket     *S3Bucket `gorethink:"bucket"`
+	Encryption bool      `gorethink:"encryption"`
 }
 
 type User struct {
-	Id string `gorethink:"id,omitempty"`
-	Domain string `gorethink:"domain"`
-	Tokens []string `gorethink:"tokens"`
-	NamingScheme string `gorethink:"naming_scheme"`
+	Id           string   `gorethink:"id,omitempty"`
+	Domain       string   `gorethink:"domain"`
+	Tokens       []string `gorethink:"tokens"`
+	NamingScheme string   `gorethink:"naming_scheme"`
 }
 
 func (user User) NewDomain(DomainName string) {
 	DomainInfo := Domain{
-		Id: DomainName,
-		Public: false,
+		Id:        DomainName,
+		Public:    false,
 		Whitelist: []string{user.Id},
 		Blacklist: []string{},
-		Owner: user.Id,
+		Owner:     user.Id,
 	}
 	err := r.Table("domains").Insert(DomainInfo).Exec(RethinkConnection)
 	if err != nil {
